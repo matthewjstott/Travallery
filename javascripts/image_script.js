f3f6ff12fd3b89e4269049e0cd92245147760247
Matthew J. Stott <matthew.stott@kcl.ac.uk>
2019-10-02 18:40:45 +0100

    Init Commit

---
+++ javascripts/image_script.js
@@ -0,0 +1,14 @@
+var root = document.getElementById('portrait');
+
+var mouse_monitor = function(e) {
+   let x = e.clientX/innerWidth;
+   let y = e.clientY/innerHeight;
+   
+   let move_x = (x>0.2) ? '-15px' : '15px';
+   let move_y = (y>0.2) ? '-10px' : '10px';
+   
+   root.style.setProperty("--translate-x", move_x);
+   root.style.setProperty("--translate-y", move_y);
+}
+
+root.addEventListener("mousemove", mouse_monitor);
